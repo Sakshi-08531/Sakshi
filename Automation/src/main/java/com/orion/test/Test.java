35dc141350834084cafe806e330469764bc0a7e9
Sakshi <Sakshi@Sakshi-PC>
2020-10-29 11:09:02 +0530

    commit

---
+++ Automation/src/main/java/com/orion/test/Test.java
@@ -0,0 +1,26 @@
+package com.orion.test;
+
+import java.util.Arrays;
+
+public class Test {
+
+	public static void main(String[] args) {
+	
+			/*for(int i=0;i<row;i++) {
+				for(int j=0;i>0;j++) {
+					System.out.println("*");*/
+		
+				int a=5;
+				int b=6;
+				
+				a=a+b;
+				b=a-b;
+				a=a-b;
+				System.out.println(a);
+				//System.out.println(b);
+			
+	}
+		 
+	
+}
+// 
